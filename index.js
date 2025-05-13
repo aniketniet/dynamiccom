702c7b8543179c91324add86ccf5ebf6d52e7d7d
Aniket Kumar Rathaur <113973457+aniketniet@users.noreply.github.com>
2025-05-13 12:21:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"], // or your frontend URL
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://dynamicecom-f.vercel.app"], // or your frontend URL
     credentials: true, // if you're using cookies
   })
 );
